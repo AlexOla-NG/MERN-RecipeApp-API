03591be5aa62cddfef8ce5bb93951a35cae0d770
Alex Oweka <alexanderolalekanoweka@gmail.com>
2023-05-01 10:01:19 +0100

    🧹fix: updated keys in user model

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,53 +3,51 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { UserModel } from "../models/Users.js";
 
-// STUB: create two endpoints for user registration and login
+// NOTE: create two endpoints for user registration and login
 
 const router = express.Router();
 
 // STUB: registration endpoint
 router.post("/register", async (req, res) => {
-	const { username, password } = req.body;
+	const { email, password } = req.body;
 
-	// STUB: check if username already exists
-	const user = await UserModel.findOne({ username });
+	// STUB: check if email already exists
+	const user = await UserModel.findOne({ email });
 
 	if (user) {
-		return res.status(400).json({ message: "Username already exists" });
+		return res.status(400).json({ message: "Email already exists" });
 	}
 
-	// STUB: if user name does not exist, create new user
+	// STUB: if email does not exist, create new user
 	const hashedPassword = await bcrypt.hash(password, 10);
-	const newUser = new UserModel({ username, password: hashedPassword });
-	newUser.save();
+	const newUser = new UserModel({ email, password: hashedPassword });
+	await newUser.save();
 
 	res.json({ message: "User successfully created" });
 });
 
 // STUB: login endpoint
 router.post("/login", async (req, res) => {
-	const { username, password } = req.body;
+	const { email, password } = req.body;
 
-	// STUB: check if username exists
-	const user = await UserModel.findOne({ username });
+	// STUB: check if email exists
+	const user = await UserModel.findOne({ email });
 
 	if (!user) {
-		return res.status(400).json({ message: "Username does not exist" });
+		return res.status(400).json({ message: "email does not exist" });
 	}
 
 	// STUB: check if password matches
 	const isPasswordValid = await bcrypt.compare(password, user.password);
 
 	if (!isPasswordValid) {
-		return res
-			.status(400)
-			.json({ message: "Incorrect username or password" });
+		return res.status(400).json({ message: "Incorrect email or password" });
 	}
 
-	// STUB: if username and password match, create JWT
+	// STUB: if email and password match, create JWT
 	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 
-	return res.json({ token, userID: user._id });
+	return res.json({ data: { token, userID: user._id } });
 });
 
 export { router as userRouter };
